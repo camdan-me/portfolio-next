4065c571736094fc9706c0abe0a39d8889004db3
Camdan Mead <hello@camdan.me>
2023-03-12 12:55:33 -0600

    ✨ gray out selected page on navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,21 @@
+import { useRouter } from 'next/router';
+
 import Image from 'next/image';
 import Link from 'next/link';
 
 import logo from '../../public/assets/img/transparent-logo.png';
 
 export default function Navbar() {
+	const router = useRouter();
+
 	return (
 		<>
 			<div className="absolute z-40 flex flex-row items-center justify-center p-4 text-2xl font-black text-gray-500 bg-white shadow-lg rounded-xl bottom-4 left-4">
 				<Link href="/?skipanimation"><Image src={logo} className="w-12 h-12 mr-2" alt="C Logo" /></Link>
-				<Link href="/about" className="mx-2 link-underline">About</Link>
-				<Link href="/projects" className="mx-2 link-underline">Projects</Link>
-				<Link href="/contact" className="mx-2 link-underline">Contact</Link>
-				<Link href="/resume" className="mx-2 link-underline">Resume</Link>
+				<Link href="/about" className={`${router.asPath === '/about' ? 'text-gray-100 cursor-default' : 'link-underline'} mx-2`}>About</Link>
+				<Link href="/projects" className={`${router.asPath === '/projects' ? 'text-gray-100 cursor-default' : 'link-underline'} mx-2`}>Projects</Link>
+				<Link href="/contact" className={`${router.asPath === '/contact' ? 'text-gray-100 cursor-default' : 'link-underline'} mx-2`}>Contact</Link>
+				<Link href="/resume" className={`${router.asPath === '/resume' ? 'text-gray-100 cursor-default' : 'link-underline'} mx-2`}>Resume</Link>
 			</div>
 		</>
 	);
