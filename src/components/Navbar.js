9405e428b6e30bd04cbd12ca60ed9567ea825b8c
Camdan Mead <hello@camdan.me>
2023-03-13 19:15:51 -0600

    ✨ base site ready for deployment

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,12 +10,13 @@ export default function Navbar() {
 
 	return (
 		<>
-			<div className="absolute z-40 flex-row items-center justify-center hidden p-4 text-2xl font-black text-white sm:flex top-4 left-4">
-				<Link href="/?skipanimation"><Image src={logo} className="w-12 h-12 mr-2" alt="C Logo" /></Link>
+			<div className="absolute z-40 flex flex-row items-center justify-center p-4 font-black text-white text-l sm:text-2xl top-4 left-4">
+				<Link href="/?skipanimation" className="hidden sm:flex"><Image src={logo} className="w-12 h-12 mr-2" alt="C Logo" /></Link>
+				<Link href="/?skipanimation" className="mx-2 link-underline sm:hidden">Home</Link>
 				<Link href="/about" className={`${router.asPath.startsWith('/about') ? 'text-red-500 cursor-default' : 'link-underline'} mx-2`}>About</Link>
-				<Link href="/projects" className={`${router.asPath.startsWith('/projects') ? 'text-red-500 cursor-default' : 'link-underline'} mx-2`}>Projects</Link>
-				<Link href="/contact" className={`${router.asPath.startsWith('/contact') ? 'text-red-500 cursor-default' : 'link-underline'} mx-2`}>Contact</Link>
-				<Link href="/resume" className={`${router.asPath.startsWith('/resume') ? 'text-red-500 cursor-default' : 'link-underline'} mx-2`}>Resume</Link>
+				<Link href="/projects" className={`${router.asPath.startsWith('/projects') ? 'text-yellow-500 cursor-default' : 'link-underline'} mx-2`}>Projects</Link>
+				<Link href="/contact" className={`${router.asPath.startsWith('/contact') ? 'text-blue-500 cursor-default' : 'link-underline'} mx-2`}>Contact</Link>
+				{/* <Link href="/resume" className={`${router.asPath.startsWith('/resume') ? 'text-red-500 cursor-default' : 'link-underline'} mx-2`}>Resume</Link> */}
 			</div>
 		</>
 	);
