cc053cd3640210be7c8f39b70a7186a4256d53c3
Camdan Mead <hello@camdan.me>
2023-05-26 21:18:46 -0600

    ✨ create separate /gift page with different copy

--- next.config.js
+++ next.config.js
@@ -7,7 +7,12 @@ module.exports = {
     return [
       {
         source: '/donate',
-        destination: 'https://donate.stripe.com/28o9ASecU10p1ziaEE',
+        destination: 'https://donate.stripe.com/7sI14mgl29wV0ve3cd',
+        permanent: false,
+      },
+      {
+        source: '/gift',
+        destination: 'https://buy.stripe.com/28o9ASecU10p1ziaEE',
         permanent: false,
       },
     ];
