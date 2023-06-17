9d5b073784f8bc0fb1abcfea2065bcd6ad937202
Camdan Mead <hello@camdan.me>
2023-06-16 20:17:03 -0600

    ✨ update footer text and remove it from index

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,10 +5,12 @@ import Link from 'next/link';
 export default function Footer() {
 	const router = useRouter();
 
+	if (router.pathname === '/') return;
 	return (
 		<>
-			<div className={`${(router.pathname === '/') && 'fixed bottom-0'} content-center w-screen px-8 py-4 font-black text-center text-white`}>
-				Created with <Link href="https://nextjs.org/" className="link-underline">NextJS</Link> and <Link href="https://tailwindcss.com/" className="link-underline">TailwindCSS</Link>. Check out the project on <Link href="https://github.com/camdan-me/portfolio-next" className="link-underline">GitHub</Link>.
+			<div className={'content-center w-screen px-8 py-4 font-black text-center text-white'}>
+				Created with <Link href="https://nextjs.org/" className="link-underline">Next</Link> and <Link href="https://tailwindcss.com/" className="link-underline">Tailwind</Link>, hosted by <Link href="https://vercel.com" className="link-underline">Vercel</Link>.
+				Check out the project on <Link href="https://github.com/camdan-me/portfolio-next" className="link-underline">GitHub</Link>.
 			</div >
 		</>
 	);
