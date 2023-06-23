0922fe43a5f1f88ca46ec9ad2729d7cb95e057a0
Camdan Mead <hello@camdan.me>
2023-06-22 21:54:36 -0600

    🐛 fix project not always redirecting

--- src/pages/projects/[project].js
+++ src/pages/projects/[project].js
@@ -11,7 +11,7 @@ import logo from '../../../public/assets/img/transparent-logo.png';
 
 export default function Home({ project }) {
 	const router = useRouter();
-	const [redirectAttempt, setRedirectAttempt] = useState(0);
+	const [loadingState, setLoadingState] = useState('Please wait...');
 
 	// get the project object from the array of projects by ID
 	const projectObject = projectsArray.filter((obj) => { return obj.id === project; })[0];
@@ -19,9 +19,8 @@ export default function Home({ project }) {
 	// redirect to the project's link
 	useEffect(() => {
 		if (projectObject && projectObject.link) router.push(`/redirect?link=${projectObject.link}`);
-		else if (redirectAttempt > 0) router.push('/projects');
-		else setRedirectAttempt(redirectAttempt + 1);
-	}, [projectObject, redirectAttempt, router]);
+		else if (projectObject && projectObject.link === '') setLoadingState(<>This project is coming soon.<br />Please check back later!</>);
+	}, [projectObject, router]);
 
 	// display an intermediate page while the redirect is happening
 	return (
@@ -29,14 +28,14 @@ export default function Home({ project }) {
 			<Head>
 				<UniversalHead description="I like to make technology do cool stuff. I'm particularly good at fixing things that are broken. Sometimes I make websites, music, videos, music videos, cool lights, or concerts." />
 
-				<title>Please Wait</title>
+				<title>{loadingState}</title>
 			</Head>
 
 			<main className="flex items-center justify-center w-screen h-screen">
 				<div className="flex flex-col items-center mx-4 sm:flex-row">
 					<Image src={logo} width={128} height={128} priority="true" className="w-24 h-24 my-4 sm:w-32 sm:h-32" alt="C Logo" />
 					<h1 className="flex flex-col text-3xl font-black text-center text-white sm:ml-4 sm:text-left sm:text-5xl">
-						Please wait...<br />
+						{loadingState}
 					</h1>
 				</div>
 			</main>
