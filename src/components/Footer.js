567d15f81cf94c1da9e9b6c1fc067e7f201fa32b
Camdan Mead <hello@camdan.me>
2023-06-23 15:09:57 -0600

    ✨ add copyright and build info to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,12 +5,24 @@ import Link from 'next/link';
 export default function Footer() {
 	const router = useRouter();
 
+	const env = process.env.VERCEL_ENV ? process.env.VERCEL_ENV : 'local';
+	const sha = process.env.VERCEL_GIT_COMMIT_SHA?.substring(0, 7) ?? '0000000';
+	const commit = process.env.VERCEL_GIT_COMMIT_MESSAGE ?? '🔃 unstaged changes';
+	const author = process.env.VERCEL_GIT_COMMIT_AUTHOR_NAME ?? 'John Doe';
+
+	const displayEnv = env.charAt(0)
+		.toUpperCase() + env.slice(1);
+
 	if (router.pathname === '/') return;
 	return (
 		<>
 			<div className={'content-center w-screen px-8 py-4 font-black text-center text-white'}>
 				Created with <Link href="https://nextjs.org/" className="link-underline">Next</Link> and <Link href="https://tailwindcss.com/" className="link-underline">Tailwind</Link>, hosted by <Link href="https://vercel.com" className="link-underline">Vercel</Link>.
-				Check out the project on <Link href="https://github.com/camdan-me/portfolio-next" className="link-underline">GitHub</Link>.
+				Check out the project on <Link href="https://github.com/camdan-me/portfolio-next" className="link-underline">GitHub</Link>.<br />
+				<div className="mt-1 text-sm text-gray-100">
+					Copyright © 2023 Camdan Mead - All Rights Reserved<br />
+					{displayEnv} build {sha} - {commit} ({author})
+				</div>
 			</div >
 		</>
 	);
