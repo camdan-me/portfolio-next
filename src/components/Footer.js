64ad0143322f21e41a15c0db5ef53f64c5e542cb
Camdan Mead <hello@camdan.me>
2023-06-23 15:14:50 -0600

    🚧 add next public prefixes to vercel env

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,16 +2,16 @@ import { useRouter } from 'next/router';
 
 import Link from 'next/link';
 
+const env = process.env.NEXT_PUBLIC_VERCEL_ENV ? process.env.VERCEL_ENV : 'local';
+const sha = process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA?.substring(0, 7) ?? '0000000';
+const commit = process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_MESSAGE ?? '🔃 unstaged changes';
+const author = process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_AUTHOR_NAME ?? 'John Doe';
+
+const displayEnv = env.charAt(0)
+	.toUpperCase() + env.slice(1);
+
 export default function Footer() {
 	const router = useRouter();
-
-	const env = process.env.VERCEL_ENV ? process.env.VERCEL_ENV : 'local';
-	const sha = process.env.VERCEL_GIT_COMMIT_SHA?.substring(0, 7) ?? '0000000';
-	const commit = process.env.VERCEL_GIT_COMMIT_MESSAGE ?? '🔃 unstaged changes';
-	const author = process.env.VERCEL_GIT_COMMIT_AUTHOR_NAME ?? 'John Doe';
-
-	const displayEnv = env.charAt(0)
-		.toUpperCase() + env.slice(1);
 
 	if (router.pathname === '/') return;
 	return (
