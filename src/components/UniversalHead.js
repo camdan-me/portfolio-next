dc6c64f3f9a437ba4d3ad47e8d5c10777d833ab2
Camdan Mead <hello@camdan.me>
2024-12-31 14:03:55 -0700

    ✨ tweak head for additional customizability

--- src/components/UniversalHead.js
+++ src/components/UniversalHead.js
@@ -1,4 +1,9 @@
 export default function UniversalHead(props) {
+	const title = props.title || 'Camdan Mead';
+	const description = props.description || 'I\'m an entertainment professional based out of Denver who specializes in lighting design and live video production.';
+	const image = props.image || '/assets/img/banner.png';
+	const color = props.color || '#2e4057';
+
 	return (
 		<>
 			<meta charSet="UTF-8" />
@@ -7,19 +12,19 @@ export default function UniversalHead(props) {
 			<meta httpEquiv="X-UA-Compatible" content="IE=edge" />
 			<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=5, shrink-to-fit=no" />
 
-			<meta name="description" content={props.description} />
+			<meta name="description" content={description} />
 
 			<meta name="twitter:card" content="summary_large_image" />
-			<meta name="twitter:title" content="Camdan Mead" />
-			<meta name="twitter:description" content={props.description} />
-			<meta name="twitter:image" content="/assets/img/banner.png" />
+			<meta name="twitter:title" content={title} />
+			<meta name="twitter:description" content={description} />
+			<meta name="twitter:image" content={image} />
 
 			<meta property="og:type" content="website" />
-			<meta property="og:title" content="Camdan Mead" />
-			<meta property="og:description" content={props.description} />
-			<meta property="og:image" content="/assets/img/banner.png" />
+			<meta property="og:title" content={title} />
+			<meta property="og:description" content={description} />
+			<meta property="og:image" content={image} />
 
-			<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#2e4057" />
+			<meta name="theme-color" content={color} />
 		</>
 	);
 }
